7eed0acf0b828087b95d27851f686c695afd8078
GabrielFogo <gabrielcostal164@gmail.com>
2024-09-05 20:41:25 -0300

    Resolução do desafio

---
+++ index.js
@@ -0,0 +1,15 @@
+//Gabriel Costal Fogo
+//3° DSM
+
+//Agora a função somar recebe os parametros para a soma e retorna o valor
+function somar (numero1, numero2){
+    return numero1 + numero2
+}
+
+//A Constante x agora esta sendo inicializada
+const x = 0, y = 5
+
+//Usando a propria função de somar para realizar a soma x + 10
+const z = somar(x,10)
+
+console.log(somar(z,y))
